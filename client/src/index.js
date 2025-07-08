119fc3bec139de961e6cafc5a5f43f6acf7c081c
utruong309 <uyenthutruong09@gmail.com>
2025-07-09 01:17:31 +0700

    change file name

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./main";
+import App from ".";
 import { AuthProvider } from "./context/AuthContext";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
