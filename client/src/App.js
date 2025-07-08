cf564cd4627a0de9797bdd4d93d3f6d84a160fad
utruong309 <uyenthutruong09@gmail.com>
2025-07-09 01:15:34 +0700

    firebase setup login logout

---
+++ client/src/App.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { useAuth } from "./context/AuthContext";
+import './App.css';
+
+function App() {
+  const { user, login, logout } = useAuth();
+
+  return (
+    <div style={{ textAlign: "center", padding: "2rem" }}>
+      {user ? (
+        <>
+          <h2>Welcome, {user.displayName}</h2>
+          <img
+            src={user.photoURL}
+            alt="avatar"
+            width={100}
+            style={{ borderRadius: "50%", margin: "1rem" }}
+          />
+          <br />
+          <button onClick={logout}>Logout</button>
+        </>
+      ) : (
+        <>
+          <h1>GigWise</h1>
+          <button onClick={login}>Sign in with Google</button>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default App;
---
+++ client/src/firebase/AuthContext.js
@@ -0,0 +1,43 @@
+import { createContext, useContext, useEffect, useState } from 'react'; 
+import app from "./config"; 
+import {
+    getAuth, 
+    GoogleAuthProvider, 
+    signInWithPopup, 
+    signOut, 
+    onAuthStateChanged
+} from "firebase/auth"; 
+
+const AuthContext = createContext(); 
+const auth = getAuth(app); 
+
+export function AuthProvider( {children} ) { //pass the entire App as children
+    const [user, setUser] = useState(null); 
+    cosnt [loading, setLoading] = useState(null); 
+
+    const login = async () => {
+        const provider = new GoogleAuthProvider(); //Google popup
+        await signInWithPopup(auth, provider); 
+    }; 
+
+    const logout = async () => {
+        await signOut(auth); 
+    }; 
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (firebaseUser) => {
+            setUser(firebaseUser); 
+            setLoading(false); 
+        }); 
+
+        return () => unsubscribe(); 
+    }, []); 
+
+    return (
+        <AuthContext.Provider value={{ user, login, logout }}>
+            {!loading && children}
+        </AuthContext.Provider>
+    ); 
+}
+
+export const useAuth = () => useContext(AuthContext); 
