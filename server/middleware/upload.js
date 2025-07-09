43efc6fd7e893cd6cfd334b157ded8bf4c12ef80
utruong309 <uyenthutruong09@gmail.com>
2025-07-10 01:49:54 +0700

    multer middleware

---
+++ server/middleware/upload.js
@@ -0,0 +1,12 @@
+import multer from 'multer';
+
+const storage = multer.diskStorage({
+  destination: 'uploads/', // save to uploads/ folder
+  filename: (_, file, cb) => {
+    cb(null, `${Date.now()}-${file.originalname}`); //1720524909035-orders.csvx
+  }
+});
+
+const upload = multer({ storage }); //multer instance, use disk storage, upload.single('csv)
+
+export default upload;
